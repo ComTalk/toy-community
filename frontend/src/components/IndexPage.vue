43b5247d72ca0a0ba4cdc03be7f5481099c42339
leesk0502 <leesk0502@gmail.com>
2021-07-25 18:01:40 +0900

    feat: [frontend] Add Example Index page using axios

---
+++ frontend/src/components/IndexPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="user">
+        <h1>로그인 완료!</h1>
+    </div>
+    <div v-else>
+        <h1>로그인 해주세요</h1>
+        <button>Google로 로그인하기</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      user: null
+    }
+  },
+  created () {
+    this.$http.get('/api/login')
+      .then(res => {
+        const user = res.data.user
+        if (user) {
+          this.user = user
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+}
+
+</script>
+
+<style></style>
